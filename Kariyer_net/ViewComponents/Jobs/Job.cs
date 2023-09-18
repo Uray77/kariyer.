b43fd38a10941a4844850ba0cc06fdb58f9e89f7
abdullah alerk <abdullahalerk@abdullahs-MacBook-Air.local>
2023-09-18 13:56:31 +0300

    Abdullah_alerk77

---
+++ Kariyer_net/ViewComponents/Jobs/Job.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kariyer_net.ViewComponents.Jobs
+{
+
+	//ViewComponent classı bizim oluşturduğumuz bir class değil.Asp.netCore yapısının kendisine ait olan bir yapısı.
+	public class Job:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+
+
+			return View();
+		}
+
+
+	}
+}
+
